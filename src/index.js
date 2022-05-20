aa72ab9f41483c1dbc68b7f61691a5d0ce87b3a7
Monika Stokowska <monikastokowska@Monikas-MBP-2.lan>
2022-05-20 06:16:41 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import React from 'react'
-import { ReactDOM } from 'react'
+import ReactDOM from 'react-dom'
+import App from "./App"
+
+// ReactDOM.render(<h1>My App</h1>, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
